ec54bede62dfe0d8c7bd0c228ec3d51a6d8aa7d8
kissesch <56185169+kissesch@users.noreply.github.com>
2025-07-30 22:36:17 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -338,6 +338,7 @@ export default {
           "suo.yt": "https://suo.yt/short",
         },
         customBackend: {
+          "自用专属后端": "https://kitsubconverter.onrender.com",
           "CM负载均衡后端【vless reality+hy1+hy2】": "https://subapi.cmliussss.net",
           "CM应急备用后端【vless reality+hy1+hy2】": "https://subapi.fxxk.dedyn.io",
           "肥羊增强型后端【vless reality+hy1+hy2】": "https://url.v1.mk",
@@ -347,6 +348,7 @@ export default {
           "sub-web作者提供": "https://api.wcc.best",
         },
         backendOptions: [
+          { value: "https://kitsubconverter.onrender.com" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -356,6 +358,15 @@ export default {
           { value: "https://api.wcc.best" },
         ],
         remoteConfig: [
+          {
+            label: "CM规则",
+            options: [
+              {
+                label: "custom",
+                value: "https://raw.githubusercontent.com/kissesch/ACL4SSR/master_custom/Clash/config/ACL_custom.ini"
+              }
+            ]
+          },
           {
             label: "CM规则",
             options: [
@@ -786,9 +797,9 @@ export default {
       form: {
         sourceSubUrl: "",
         clientType: "",
-        customBackend: this.getUrlParam() == "" ? "https://url.v1.mk" : this.getUrlParam(),
+        customBackend: this。getUrlParam() == "" ? "https://kitsubconverter.onrender.com" : this.getUrlParam(),
         shortType: "https://v1.mk/short",
-        remoteConfig: "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online.ini",
+        remoteConfig: "https://raw.githubusercontent.com/kissesch/ACL4SSR/master_custom/Clash/config/ACL_custom.ini",
         excludeRemarks: "",
         includeRemarks: "",
         filename: "",
