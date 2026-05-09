deecf446147b8b78a725b75bb87596f73a95311a
kissesch <56185169+kissesch@users.noreply.github.com>
2026-05-09 21:18:49 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -348,12 +348,14 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
+          "kits自用zocomputer": "https://subzocomputer.9698931.xyz",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
           "肥羊提供-备用后端": "https://api.v1.mk",
         },
         backendOptions: [
+          { value: "https://subzocomputer.9698931.xyz" },
           { value: "https://subapi.cmliussss.net" },
           { value: "https://subapi.fxxk.dedyn.io" },
           { value: "https://url.v1.mk" },
@@ -361,6 +363,26 @@ export default {
         ],
         remoteConfig: [
           {
+             label: "kitscustom规则",
+            options: [
+              {
+                label: "custom",
+                value: "https://raw.githubusercontent.com/kissesch/ACL4SSR/refs/heads/master_custom/Clash/config/ACL_custom.ini"
+              },
+              {
+                label: "custom_cf",
+                value: "https://raw.githubusercontent.com/kissesch/ACL4SSR/refs/heads/master_custom/Clash/config/Custom_cf.ini"
+              },
+              {
+                label: "custom_music",
+                value: "https://raw.githubusercontent.com/kissesch/ACL4SSR/refs/heads/master_custom/Clash/config/ACL_custom_musik.ini"
+              },
+              {
+                label: "custom_full",
+                value: "https://raw.githubusercontent.com/kissesch/ACL4SSR/refs/heads/master_custom/Clash/config/ACL_custom_full.ini"
+              }
+            ]
+          },
             label: "CM规则",
             options: [
               {
